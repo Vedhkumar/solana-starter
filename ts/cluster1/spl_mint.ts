e5fddd7375da68b2f63d57345c447465ac0a285f
Vedh Kumar <vedhkumar237@gmail.com>
2025-07-02 13:49:26 +0530

    spl_mint

--- ts/cluster1/spl_mint.ts
+++ ts/cluster1/spl_mint.ts
@@ -5,32 +5,47 @@ import {
   mintTo,
 } from "@solana/spl-token";
 import wallet from "../turbin3-wallet.json";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const RPC_URL = process.env.RPC_URL;
+if (!RPC_URL) {
+  throw new Error("NO RPC URL");
+}
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
 //Create a Solana devnet connection
 const commitment: Commitment = "confirmed";
-const connection = new Connection("https://api.devnet.solana.com", commitment);
+const connection = new Connection(RPC_URL, commitment);
 
 const token_decimals = 1_000_000n;
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("FunqQXZvXfA1KoNFjaazzaKu9bz8W4h38sYkLNEZEfUR");
 
 (async () => {
   try {
     // Create an ATA
-    // const ata: Account = await getOrCreateAssociatedTokenAccount(
-    //   connection,
-    //   keypair,
-    //   mint,
-    //   keypair.publicKey
-    // );
-    // console.log(`Your ata is: ${ata.address}`);
+    const ata: Account = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      keypair.publicKey
+    );
+    console.log(`Your ata is: ${ata.address}`); // HcfpqMeheKbEpmKWKtZPvBz2qjzWBkLMLXLa4SAkNvD5
+
     // Mint to ATA
-    // const mintTx = ???
-    // console.log(`Your mint txid: ${mintTx}`);
+    const mintTx = await mintTo(
+      connection,
+      keypair,
+      mint,
+      ata.address,
+      keypair,
+      5n * token_decimals
+    );
+    console.log(`Your mint txid: ${mintTx}`);
   } catch (error) {
     console.log(`Oops, something went wrong: ${error}`);
   }

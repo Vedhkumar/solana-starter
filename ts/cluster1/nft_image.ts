6599e3e1044adeaa10cc621b290d5a390e6ba3ef
Vedh Kumar <vedhkumar237@gmail.com>
2025-07-08 17:59:29 +0530

    rug day

--- ts/cluster1/nft_image.ts
+++ ts/cluster1/nft_image.ts
@@ -1,30 +1,41 @@
-import wallet from "../turbin3-wallet.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
-import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
-import { readFile } from "fs/promises"
+import wallet from "../turbin3-wallet.json";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createGenericFile,
+  createSignerFromKeypair,
+  signerIdentity,
+} from "@metaplex-foundation/umi";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+import { readFile } from "fs/promises";
+import dotenv from "dotenv";
 
+dotenv.config();
+const RPC_URL = process.env.RPC_URL;
+if (!RPC_URL) {
+  throw new Error("");
+}
 // Create a devnet connection
-const umi = createUmi('https://api.devnet.solana.com');
+const umi = createUmi(RPC_URL);
+umi.use(irysUploader({ address: "https://devnet.irys.xyz" }));
 
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
 
-umi.use(irysUploader());
 umi.use(signerIdentity(signer));
 
 (async () => {
-    try {
-        //1. Load image
-        //2. Convert image to generic file.
-        //3. Upload image
+  try {
+    //1. Load image
+    //2. Convert image to generic file.
+    //3. Upload image
 
-        // const image = ???
-
-        // const [myUri] = ??? 
-        // console.log("Your image URI: ", myUri);
-    }
-    catch(error) {
-        console.log("Oops.. Something went wrong", error);
-    }
+    const image = await readFile("./generug.png");
+    const file = createGenericFile(image, "generug.jpg", {
+      contentType: "image/png",
+    });
+    const [myUri] = await umi.uploader.upload([file]);
+    console.log("Your image URI: ", myUri); // https://gateway.irys.xyz/DC2UdZ2SGDGjtrGwYd9xaWbWzW8vxhGqSMKvJpn4XVEx
+  } catch (error) {
+    console.log("Oops.. Something went wrong", error);
+  }
 })();
--- ts/cluster1/nft_metadata.ts
+++ ts/cluster1/nft_metadata.ts
@@ -1,45 +1,51 @@
-import wallet from "../turbin3-wallet.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
-import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
+import wallet from "../turbin3-wallet.json";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createGenericFile,
+  createSignerFromKeypair,
+  signerIdentity,
+} from "@metaplex-foundation/umi";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+import dotenv from "dotenv";
 
+dotenv.config();
+const RPC_URL = process.env.RPC_URL;
+if (!RPC_URL) {
+  throw new Error("");
+}
 // Create a devnet connection
-const umi = createUmi('https://api.devnet.solana.com');
-
+const umi = createUmi(RPC_URL);
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
 
-umi.use(irysUploader());
+umi.use(irysUploader({ address: "https://devnet.irys.xyz/" }));
 umi.use(signerIdentity(signer));
 
 (async () => {
-    try {
-        // Follow this JSON structure
-        // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
+  try {
+    // Follow this JSON structure
+    // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
+    const image = "";
+    const metadata = {
+      name: "NFT",
+      symbol: "TT",
+      description: "It is just a regular nft",
+      image,
+      attributes: [{ trait_type: "general", value: "regular" }],
+      properties: {
+        files: [
+          {
+            type: "image/png",
+            uri: "https://gateway.irys.xyz/DC2UdZ2SGDGjtrGwYd9xaWbWzW8vxhGqSMKvJpn4XVEx",
+          },
+        ],
+      },
+      creators: [],
+    };
+    const myUri = await umi.uploader.uploadJson(metadata);
 
-        // const image = ???
-        // const metadata = {
-        //     name: "?",
-        //     symbol: "?",
-        //     description: "?",
-        //     image: "?",
-        //     attributes: [
-        //         {trait_type: '?', value: '?'}
-        //     ],
-        //     properties: {
-        //         files: [
-        //             {
-        //                 type: "image/png",
-        //                 uri: "?"
-        //             },
-        //         ]
-        //     },
-        //     creators: []
-        // };
-        // const myUri = ???
-        // console.log("Your metadata URI: ", myUri);
-    }
-    catch(error) {
-        console.log("Oops.. Something went wrong", error);
-    }
+    console.log("Your metadata URI: ", myUri); // https://gateway.irys.xyz/DEqFSibPDM55CdQiuLABAUHyrqgJAJYwtLW7pLq98QKN
+  } catch (error) {
+    console.log("Oops.. Something went wrong", error);
+  }
 })();
